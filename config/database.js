60069525a4f70cb3b191e1630e2b199aa1d60422
Kirill Vorobiov <mishkacod@gmail.com>
2023-01-04 00:13:31 +0200

    update database

--- config/database.js
+++ config/database.js
@@ -1,11 +1,44 @@
-const path = require('path');
+const path = require("path");
 
-module.exports = ({ env }) => ({
-  connection: {
-    client: 'sqlite',
+module.exports = ({ env }) => {
+
+  if (env("NODE_ENV") === "production") {
+
+    return {
+      defaultConnection: "default",
+      connections: {
+        default: {
+          connector: "bookshelf",
+          settings: {
+            client: "postgres",
+            host: env("DATABASE_HOST"),
+            port: env("DATABASE_PORT"),
+            database: env("DATABASE_NAME"),
+            username: env("DATABASE_USER"),
+            password: env("DATABASE_PASS"),
+            ssl: {
+              rejectUnauthorized: false,
+            },
+          },
+          options: {
+            ssl: false,
+          },
+        },
+      },
+    };
+  }
+
+  return {
     connection: {
-      filename: path.join(__dirname, '..', env('DATABASE_FILENAME', '.tmp/data.db')),
+      client: "sqlite",
+      connection: {
+        filename: path.join(
+          __dirname,
+          "..",
+          env("DATABASE_FILENAME", ".tmp/data.db")
+        ),
+      },
+      useNullAsDefault: true,
     },
-    useNullAsDefault: true,
-  },
-});
+  };
+};
