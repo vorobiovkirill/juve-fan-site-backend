4051610d42b34d44660d1accbce61e26b3d5b571
Kirill Vorobiov <mishkacod@gmail.com>
2023-01-04 00:21:23 +0200

    update server js

--- config/server.js
+++ config/server.js
@@ -1,7 +1,15 @@
-module.exports = ({ env }) => ({
-  host: env('HOST', '0.0.0.0'),
-  port: env.int('PORT', 1337),
-  app: {
-    keys: env.array('APP_KEYS'),
-  },
-});
+module.exports = ({ env }) => {
+  if (env("NODE_ENV") === "production") {
+    return {
+      url: env("MY_HEROKU_URL"),
+    };
+  }
+
+  return {
+    host: env("HOST", "0.0.0.0"),
+    port: env.int("PORT", 1337),
+    app: {
+      keys: env.array("APP_KEYS"),
+    },
+  };
+};
