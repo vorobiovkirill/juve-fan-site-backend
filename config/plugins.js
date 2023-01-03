7478afde8889bff53fe2b816d3341fe3681aa18b
Kirill Vorobiov <mishkacod@gmail.com>
2023-01-03 21:47:41 +0200

    add graphql plugin

--- config/plugins.js
+++ config/plugins.js
@@ -16,4 +16,16 @@ module.exports = ({ env }) => ({
         },
       },
     },
+    graphql: {
+      config: {
+        endpoint: '/graphql',
+        shadowCRUD: true,
+        playgroundAlways: true,
+        depthLimit: 7,
+        amountLimit: 100,
+        apolloServer: {
+          tracing: false,
+        },
+      },
+    },
   });
