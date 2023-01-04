af249e3b4f62c008ef8e95eaf158441d9e60c0a8
Kirill Vorobiov <mishkacod@gmail.com>
2023-01-04 11:41:44 +0200

    Update database config

---
+++ config/env/production/database.js
@@ -0,0 +1,22 @@
+module.exports = ({ env }) => ({
+  defaultConnection: "default",
+  connections: {
+    default: {
+      connector: "bookshelf",
+      settings: {
+        client: "postgres",
+        host: env("DATABASE_HOST"),
+        port: env("DATABASE_PORT"),
+        database: env("DATABASE_NAME"),
+        username: env("DATABASE_USER"),
+        password: env("DATABASE_PASS"),
+        ssl: {
+          rejectUnauthorized: false,
+        },
+      },
+      options: {
+        ssl: false,
+      },
+    },
+  },
+});
