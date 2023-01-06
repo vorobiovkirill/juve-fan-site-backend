33f90ebd1f671af932ce53fce8e2d1b69a4a6ccb
Kirill Vorobiov <mishkacod@gmail.com>
2023-01-06 17:42:17 +0200

    add slugify plugin

--- config/plugins.js
+++ config/plugins.js
@@ -1,14 +1,25 @@
 module.exports = ({ env }) => ({
-    graphql: {
-      config: {
-        endpoint: '/graphql',
-        shadowCRUD: true,
-        playgroundAlways: true,
-        depthLimit: 7,
-        amountLimit: 100,
-        apolloServer: {
-          tracing: false,
+  slugify: {
+    enabled: true,
+    config: {
+      contentTypes: {
+        article: {
+          field: "slug",
+          references: "title",
         },
       },
     },
-  });
+  },
+  graphql: {
+    config: {
+      endpoint: "/graphql",
+      shadowCRUD: true,
+      playgroundAlways: true,
+      depthLimit: 7,
+      amountLimit: 100,
+      apolloServer: {
+        tracing: false,
+      },
+    },
+  },
+});
