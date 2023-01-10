2ba71fe1cd5abb0e8bc0b2561f46508b03b1c3e3
Kirill Vorobiov <mishkacod@gmail.com>
2023-01-10 10:32:12 +0200

    add cloudinary upload plugin

--- config/middlewares.js
+++ config/middlewares.js
@@ -1,7 +1,20 @@
 module.exports = ({ env }) => {
   return [
     "strapi::errors",
-    "strapi::security",
+    {
+      name: 'strapi::security',
+      config: {
+        contentSecurityPolicy: {
+          useDefaults: true,
+          directives: {
+            'connect-src': ["'self'", 'https:'],
+            'img-src': ["'self'", 'data:', 'blob:', 'dl.airtable.com', 'res.cloudinary.com'],
+            'media-src': ["'self'", 'data:', 'blob:', 'dl.airtable.com', 'res.cloudinary.com'],
+            upgradeInsecureRequests: null,
+          },
+        },
+      },
+    },
     "strapi::cors",
     "strapi::poweredBy",
     "strapi::logger",
